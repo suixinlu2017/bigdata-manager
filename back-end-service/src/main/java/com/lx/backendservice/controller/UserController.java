3eb6cfe690c7229278a7b606ac9c82189f8e5ffd
chenyulong <872626491@qq.com>
2019-07-23 14:54:46 +0800

    初始化前端系统

--- back-end-service/src/main/java/com/lx/backendservice/controller/UserController.java
+++ back-end-service/src/main/java/com/lx/backendservice/controller/UserController.java
@@ -2,11 +2,15 @@ package com.lx.backendservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @program: bigdata-conosle
@@ -30,5 +34,11 @@ public class UserController {
     public String login(String username, String password) {
         System.out.println("-----------------");
         return "login ok " + username + password + new Date();
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("timestamp:" + System.currentTimeMillis());
+        return "timestamp:" + System.currentTimeMillis();
     }
 }
